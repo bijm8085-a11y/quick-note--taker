16e7c73f1f1ecc9d3e5bd01a315ebe9a17d91600
bijm8085-a11y <bijm8085@gmail.com>
2026-04-28 11:55:37 +0900

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    saveNote: (text, filePath) => ipcRenderer.invoke('save-note', text, filePath),
+    loadNote: () => ipcRenderer.invoke('load-note'),
+    saveAs: (text) => ipcRenderer.invoke('save-as', text),
+    newNote: () => ipcRenderer.invoke('new-note'),
+    deleteAll: () => ipcRenderer.invoke('delete-all'),
+    openFile: () => ipcRenderer.invoke('open-file')
+});
